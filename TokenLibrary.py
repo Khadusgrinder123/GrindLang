61ef62d51276a7a24354f69a45d49cad48c3847b
YajatSingh <1opgrinder@gmail.com>
2026-09-21 23:34:33 +0530

    Update TokenLibrary.py

--- TokenLibrary.py
+++ TokenLibrary.py
@@ -17,24 +17,10 @@ class TokenType:
             "reps": "REPS", # expects a number after this
             "keep": "KEEP", # this is only used after reps
             # creates infinite loop
-            "until": "KEEP_UNTIL", # put a boolean here to create a infinite loop
-            # loop will run until some value is true
             "func": "FUNCTION",
-            "class": "CLASS",
-            "length": "LENGTH_OF_STRING",
-            "allocate": "ALLOCATE",
             "nil": "NILL",
             "delete": "DELETE",
             "import": "IMPORT",
-            "try": "TRY",
-            "catch": "CATCH",
-            "throw": "THROW",
-            "switch": "SWITCH",
-            "case": "CASE",
-            "default": "DEFAULT",
-            "force": "FORCE", # variable wont be deleted until manually removed
-            "nickname": "NICKNAME", # typedef or alias
-            "assume": "ASSUME", # assert expects something throw error if false
             "return": "RETURN",
             "break": "BREAK",
             "continue": "CONTINUE",
@@ -45,33 +31,24 @@ class TokenType:
             "true": "TRUE",
             "false": "FALSE",
             "let": "LET",
-            "const": "CONST",
-            "sizeof": "SIZEOF",
             "show": "PRINT",
 
+
             # data types
-            "tiny_int": "INT8",
-            "small_int": "INT16",
-            "mid_int": "INT32",
-            "large_int": "INT64",
-            "tiny_flt": "FLOAT16",
-            "small_flt": "FLOAT32",
-            "mid_flt": "FLOAT64",
-            "large_flt": "FLOAT128",
+            "int": "INTEGER",
+            "float":"FLOAT",
             "bool": "BOOLEAN",
-            "char": "CHARACTER",
             "str": "STRING"   
         }
         return self.Keywords
 
+    
     def NonLetter(self):
         self.NonLetters = {
             "(": "LPAREN",
             ")": "RPAREN",
             "{": "LCURLY",
             "}": "RCURLY",
-            "[": "LSQUARE",
-            "]": "RSQUARE",
             ":": "COLON",
             ";": "END_OF_STATEMENT",
             "\n": "END_OF_STATEMENT",
@@ -87,7 +64,6 @@ class TokenType:
             "*": "*",
             "/": "/",
             "=": "EQUAL_TO",
-            "&": "ADRESSOF",
             "!": "NOT",
             "<": "LESS_THAN",
             ">": "GREATER_THAN"
